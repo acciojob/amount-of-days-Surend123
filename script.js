be2dee3b5ce369786303c8ef289b49e32965a78a
Surendra <sky958447@gmail.com>
2025-01-14 18:47:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function dayOfAYear(value){
+	if(value%100 ==0){
+		if(value%400 ==0){
+			return 366;
+		}else{
+			return 365;
+		}
+	}else if(value%4==0){
+		return 366;
+	}else{
+		return 365;
+	}
+}
+
+let value = prompt("Enter valid Year");
+alert(dayOfAYear(value));
